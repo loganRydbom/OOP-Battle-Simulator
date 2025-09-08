cfac7850576b839ef0466afb0455be84f71a46dd
loganRydbom <jimrobert200@gmail.com>
2025-09-08 11:11:31 -0500

    Update uther.py

--- uther.py
+++ uther.py
@@ -2,7 +2,15 @@ import random
 from enemy import Enemy
 
 class Uther(Enemy):
+    def __init__(self, name):
+        super().__init__(name)
+        self.holyPower = 0
     def attack(self):
+        if self.holyPower > 10:
+            self.health += 10
+            self.holyPower = 0
+        else:
+            self.holyPower += random.randint(1, 3)
         return random.randint(1, self.attack_power) * 1.25
 
     def take_damage(self, damage):
